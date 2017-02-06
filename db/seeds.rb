25859e3bf87a4e86f7e4a51baecfd149f9c9f7d4
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2017-02-06 12:01:49 -0800

    Organize scss files. Main scss handles import order

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
     title: "Test#{i}",
     user_id: 5,
     audio_url: "https://res.cloudinary.com/flikr/video/upload/v1486323594/automation_bwwogm.mp3",
-    cover_photo: "https://res.cloudinary.com/flikr/image/upload/c_scale,w_214/v1480896511/oi18tltq5yxmuweme5y1.jpg",
+    cover_photo: "https://res.cloudinary.com/flikr/image/upload/v1486411231/cover_o90m2w.jpg",
     genre: "Test",
     artist: "Napoleon"
     })
