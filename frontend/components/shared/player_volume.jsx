d091448a1e48bb2f1696dd578270325e3819ad85
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2017-02-09 11:47:09 -0800

    Volume working

--- frontend/components/shared/player_volume.jsx
+++ frontend/components/shared/player_volume.jsx
@@ -6,6 +6,15 @@ export default class PlayerVolume extends React.Component {
     this.state = {volume: 1};
   }
 
+  componentDidMount() {
+    let width = 20;
+    for (var i = 10; i > 0 ; i--) {
+      $(`#${i}`).css('width', `${width}px`);
+      $(`#${i}`).addClass('volume');
+      width -= 2;
+    }
+  }
+
   startDrag(e){
     e.dataTransfer.setDragImage(this.volumeHandle, -99999, -99999);
   }
@@ -34,26 +43,38 @@ export default class PlayerVolume extends React.Component {
     this.props.volumeTo(normalizedY / 100);
   }
 
+  volume(volume) {
+    let ids = [];
+    for (let i = 1; i <= volume * 10; i++) ids.push(i);
+    for (let id = 1; id <= 10; id++) {
+      if (!ids.includes(id)) {
+        $(`#${id}`).addClass('volume-off');
+      } else {
+        $(`#${id}`).removeClass('volume-off');
+      }
+    }
+    this.props.volumeTo(volume);
+  }
+
   render() {
     return(
       <div className='player-volume'>
-        <button className='volume-control'></button>
+        <button
+          className='volume-control'
+          onClick={() => this.volume(0)}>
+        </button>
         <div className='volume-slider' >
-          <div
-            className='volume-slider-wrapper'
-            draggable={true}
-            onDrag={e => this.dragging(e)}
-            onDragStart={e => this.startDrag(e)}
-            onDragEnd={e => this.endDrag(e)}>
-            <div className='volume-slider-bar'></div>
-            <div
-              className='volume-slider-progress'
-              ref={volumeBar => this.volumeBar = volumeBar}>
-            </div>
-            <div
-              className='volume-slider-handle'
-              ref={volumeHandle => this.volumeHandle = volumeHandle}>
-            </div>
+          <div className='volume-slider-wrapper'>
+            <div id='10' onClick={() => this.volume(1)}></div>
+            <div id='9' onClick={() => this.volume(0.9)}></div>
+            <div id='8' onClick={() => this.volume(0.8)}></div>
+            <div id='7' onClick={() => this.volume(0.7)}></div>
+            <div id='6' onClick={() => this.volume(0.6)}></div>
+            <div id='5' onClick={() => this.volume(0.5)}></div>
+            <div id='4' onClick={() => this.volume(0.4)}></div>
+            <div id='3' onClick={() => this.volume(0.3)}></div>
+            <div id='2' onClick={() => this.volume(0.2)}></div>
+            <div id='1' onClick={() => this.volume(0.1)}></div>
           </div>
         </div>
       </div>
