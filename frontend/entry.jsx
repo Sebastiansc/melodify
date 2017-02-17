7a99e516cb08da3a1ff4ef8ff49ef0636b4c894e
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2017-02-17 15:07:25 -0800

    Testing grade dj deploy error

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
 import Root from './components/root.jsx';
 import Modal from 'react-modal';
-import Grade from 'grade-js';
+// import Grade from 'grade-js';
 
 document.addEventListener("DOMContentLoaded", () => {
 
