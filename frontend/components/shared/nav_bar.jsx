fc0ae1f6886685498835ee6413ba4f3186d76b84
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2017-02-25 22:25:34 -0800

    Organize nav bar jsx

--- frontend/components/shared/nav_bar.jsx
+++ frontend/components/shared/nav_bar.jsx
@@ -26,136 +26,140 @@ export default class Navbar extends React.Component {
     });
   }
 
+  loggedInNavBar() {
+    return(
+      <div className='container navbar-wrapper'>
+        <div className='row navbar-row'>
+
+          <div
+            className='navbar-logo
+            col-lg-1
+            col-md-1
+            col-sm-1'
+            >
+            <Link to='/'>Melodify</Link>
+          </div>
+
+          <ul
+            className='left-navbar-wrapper
+            col-lg-2
+            col-md-2
+            col-sm-2'
+            >
+            <li><Link to='charts/top'>Charts</Link></li>
+            <li><Link to='collection'>Collection</Link></li>
+          </ul>
+
+          <SearchBarContainer
+            formClass='navbar-search'
+            inputClass='navbar-searchfield'
+            buttonClass='navbar-searchbutton'
+            className='col-lg-5
+            col-md-5
+            col-sm-5'
+            />
+
+          <div
+            className='upload-wrapper
+            col-lg-1
+            col-md-1
+            col-sm-1'>
+            <Link to='upload' className='navbar-upload'>Upload</Link>
+          </div>
+
+          <div
+            className='profile-menu
+            col-lg-2
+            col-md-2
+            col-sm-2'>
+            <div
+              to='#'
+              className={`profile-menu-link`}>
+              <span className='profile-menu-pic'></span>
+              <div className='profile-menu-name'>
+                <span>{this.props.user.username}</span>
+                <span className='menu-arrow'></span>
+              </div>
+              <div className={ `profile-menu-dropdown`}>
+                <ul className='dropdown-list'>
+                  <Link className='profile-link'>Profile</Link>
+                  <Link className='likes-link'>Likes</Link>
+                  <Link className='playlists-link'>Playlists</Link>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <SessionButtonsContainer
+            className='
+            col-lg-1
+            col-md-1
+            col-sm-1'
+            />
+        </div>
+      </div>
+    );
+  }
+
+  loggedOffNavBar() {
+    return(
+      <div className='container navbar-wrapper'>
+        <div className='row navbar-row'>
+          <div
+            className='navbar-logo
+            col-lg-2
+            col-md-2
+            col-sm-1'
+            >
+            <Link to='/'>Melodify</Link>
+          </div>
+          <ul
+            className='left-navbar-wrapper
+            col-lg-1
+            col-md-1
+            col-sm-1'
+            >
+            <li><Link to='charts/top'>Charts</Link></li>
+          </ul>
+          <SearchBarContainer
+            formClass='navbar-search'
+            inputClass='navbar-searchfield'
+            buttonClass='navbar-searchbutton'
+            className='col-lg-6
+            col-md-5
+            col-sm-5'
+            />
+          <SessionButtonsContainer
+            className='navbar-session
+            col-lg-2
+            col-md-3
+            col-sm-4'
+            />
+          <div
+            className='
+            upload-wrapper
+            small-upload
+            col-lg-1
+            col-md-1
+            col-sm-1'>
+            <Link to='upload' className='navbar-upload'>Upload</Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render(){
-    if (!this.isLoggedIn()) {
-      return(
-        <nav
-          role="navigation"
-          className="nav-bar"
-          style={{display: `${this.props.display}`}}>
-          <div className='container navbar-wrapper'>
-            <div className='row navbar-row'>
-              <div
-                className='navbar-logo
-                col-lg-2
-                col-md-2
-                col-sm-1'
-                >
-                <Link to='/'>Melodify</Link>
-              </div>
-              <ul
-                className='left-navbar-wrapper
-                col-lg-1
-                col-md-1
-                col-sm-1'
-                >
-                <li><Link to='charts/top'>Charts</Link></li>
-              </ul>
-              <SearchBarContainer
-                formClass='navbar-search'
-                inputClass='navbar-searchfield'
-                buttonClass='navbar-searchbutton'
-                className='col-lg-6
-                col-md-5
-                col-sm-5'
-                />
-              <SessionButtonsContainer
-                className='navbar-session
-                col-lg-2
-                col-md-3
-                col-sm-4'
-                />
-              <div
-                className='
-                upload-wrapper
-                small-upload
-                col-lg-1
-                col-md-1
-                col-sm-1'>
-                <Link to='upload' className='navbar-upload'>Upload</Link>
-              </div>
-            </div>
-          </div>
-        </nav>
-      );
-    } else {
-      return(
-        <nav
-          role="navigation"
-          className="nav-bar"
-          style={{display: `${this.props.display}`}}>
-          <div className='container navbar-wrapper'>
-            <div className='row navbar-row'>
-
-              <div
-                className='navbar-logo
-                col-lg-1
-                col-md-1
-                col-sm-1'
-                >
-                <Link to='/'>Melodify</Link>
-              </div>
-
-              <ul
-                className='left-navbar-wrapper
-                col-lg-2
-                col-md-2
-                col-sm-2'
-                >
-                <li><Link to='charts/top'>Charts</Link></li>
-                <li><Link to='collection'>Collection</Link></li>
-              </ul>
-
-              <SearchBarContainer
-                formClass='navbar-search'
-                inputClass='navbar-searchfield'
-                buttonClass='navbar-searchbutton'
-                className='col-lg-5
-                col-md-5
-                col-sm-5'
-                />
-
-              <div
-                className='upload-wrapper
-                col-lg-1
-                col-md-1
-                col-sm-1'>
-                <Link to='upload' className='navbar-upload'>Upload</Link>
-              </div>
-
-              <div
-                className='profile-menu
-                col-lg-2
-                col-md-2
-                col-sm-2'>
-                <div
-                  to='#'
-                  className={`profile-menu-link`}>
-                  <span className='profile-menu-pic'></span>
-                  <div className='profile-menu-name'>
-                    <span>{this.props.user.username}</span>
-                    <span className='menu-arrow'></span>
-                  </div>
-                  <div className={ `profile-menu-dropdown`}>
-                    <ul className='dropdown-list'>
-                      <Link className='profile-link'>Profile</Link>
-                      <Link className='likes-link'>Likes</Link>
-                      <Link className='playlists-link'>Playlists</Link>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-
-              <SessionButtonsContainer
-                className='
-                col-lg-1
-                col-md-1
-                col-sm-1'
-                />
-            </div>
-          </div>
-        </nav>
-      );
-    }
+    let content = this.isLoggedIn() ?
+                  this.loggedInNavBar() :
+                  this.loggedOffNavBar();
+    return (
+      <nav
+        role="navigation"
+        className="nav-bar"
+        style={{display: `${this.props.display}`}}>
+        {content}
+      </nav>
+    );
   }
 }
