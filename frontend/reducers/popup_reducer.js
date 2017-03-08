0f8d4403e3f5eddfc20548f9faf026016c9a885a
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2017-03-07 23:27:11 -0800

    Add detail styling to playlist modal

--- frontend/reducers/popup_reducer.js
+++ frontend/reducers/popup_reducer.js
@@ -2,10 +2,13 @@ import { RECEIVE_LIKE } from '../actions/like_actions';
 import merge from 'lodash/merge';
 
 const PopupReducer = (state = {}, action) => {
+  let newState = merge({}, state);
+
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_LIKE:
-      return action.track;
+      newState.track = action.track;
+      return newState;
     default:
       return state;
   }
